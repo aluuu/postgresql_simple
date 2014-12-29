e04d3d702a32442ac5b5247e3546e5d3308d3382
aluuu <aluuu@husa.su>
2014-12-29 11:54:54 +0500

    fix postgresql server setup: Travis CI supports it out of the box

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -13,8 +13,7 @@ case "$OCAML_VERSION,$OPAM_VERSION" in
 esac
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam postgresql
-sudo service postgresql start
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 export OPAMYES=1
 export OPAMVERBOSE=1
 echo OCaml version
